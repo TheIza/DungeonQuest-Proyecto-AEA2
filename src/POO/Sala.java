e9f134292a76c7d60c28319a46b0b0aad91b6a82
izarbe <izarbelanuza06@gmail.com>
2026-04-16 17:08:13 +0200

    sala actualizada

--- src/POO/Sala.java
+++ src/POO/Sala.java
@@ -3,13 +3,18 @@ package POO;
 public abstract class Sala {
 
 	protected Tresor tresor;
+	protected boolean hayTesoro;
 	protected Monstre monstre;
+	protected boolean hayMonstruo;
 	protected boolean explorada;
 	
 	public Sala(Tresor tresor, Monstre monstre, boolean explorada) {
 		this.tresor = tresor;
 		this.monstre = monstre;
 		this.explorada = explorada;
+		
+		this.hayTesoro = valorRandom();
+		this.hayMonstruo = valorRandom();
 	}
 	
 	public Tresor getTresor() {
@@ -28,10 +33,17 @@ public abstract class Sala {
 		this.monstre = monstre;
 	}
 
+	public boolean ishayTesoro() {
+		return hayTesoro;
+	}
+	
+	public boolean ishayMonstruo() {
+		return hayMonstruo;
+	}
+	
 	public boolean isExplorada() {
 		return explorada;
 	}
-
 	public void setExplorada(boolean explorada) {
 		this.explorada = explorada;
 	}
@@ -43,5 +55,15 @@ public abstract class Sala {
 	@Override
 	public String toString() {
 		return "Sala [tresor=" + tresor + ", monstre=" + monstre + ", explorada=" + explorada + "]";
+	}
+	
+	public boolean valorRandom() {
+		
+		if((int)(Math.random() * 2) == 0) {
+			return false; // si es 0 es false
+		} else {
+			return true; // si es 1 es true
+		}
+		
 	}
 }

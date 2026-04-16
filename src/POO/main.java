873dc27d3d58077e69416424344e2f1bea45e729
Marcos <mrcsjalca@gmail.com>
2026-04-16 16:52:09 +0200

    cambios enanos en el main

--- src/POO/main.java
+++ src/POO/main.java
@@ -1,32 +1,31 @@
 package POO;
-
+import java.util.ArrayList;
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Tresor o = new Tresor ("culo", 1, 2.22);
-		System.out.println(o);
-	
 		Monstre p = new Monstre ("Oscar Odena", 3, 1);
-		System.out.println(p);
-		int agilitat = 5;
-		int forsa = 8;
-		Personatge d = new Personatge ("Pepe", 3, 3, agilitat, forsa);
+		Personatge d = new Personatge ("Pepe");
 		Sala sw = new SalaTeranyina (o, p, false);
 		
-		System.out.println(sw.intentarSortir(agilitat));
 		
-		Masmorra(o, p);
+		Masmorra(d, o, p);
 	
 	}
 
 
-	public static void Masmorra (Tresor tresor, Monstre monstre) {
+	public static void Masmorra (Personatge personatge,Tresor tresor, Monstre monstre) {
 
+		int[] arrayPersonatge = personatge.getPosicio();
+		boolean d = false;
 		
 		for (int fila = 0; fila < 6; fila++) {
+			d = arrayPersonatge[0] == fila;
 			for (int columna = 0; columna < 6; columna++) {
-				int Salas = (int) (Math.random() * 10) +1;
+				int Salas = (int) (Math.random() * 20) +1;
+
 				boolean SalaTeranyina = false;
 				boolean SalaPont = false;
 				boolean SalaComuna = false;
@@ -35,16 +34,20 @@ public class main {
 					SalaTeranyina = true;
 				} else if (Salas == 3 || Salas == 4){
 					SalaPont = true;
-				} else {
+				} else if (Salas > 4 && Salas <= 10){
 					SalaComuna = true;
 				}
 				
-				if (SalaTeranyina) {
+				if (d && arrayPersonatge[1] == columna) {
+					System.out.print("&");
+				} else if (SalaTeranyina) {
 					System.out.print("1");
 				} else if (SalaPont) {
 					System.out.print("2");
 				} else if (SalaComuna){
 					System.out.print("3");
+				} else {
+					System.out.print("-");
 				}
 				
 				
